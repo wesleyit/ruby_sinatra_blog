58b24b48f0553e8527f17f643c683af1e0579fee
Computador <computador@localhost.localdomain>
2015-02-01 20:32:52 -0200

    Incluído o suporte a Markdown
    Adicionada a extensão rdiscount para
    dar suporte aos posts em Markdown.
    Os posts em erb foram excluídos.

--- main.rb
+++ main.rb
@@ -1,11 +1,17 @@
 # encoding: UTF-8
 require 'sinatra'
 require 'find'
+require 'rdiscount'
+
+
+# for all markdown files, use post.haml as layout
+#set :markdown, :layout_engine => :erb, :layout => :post
+
 
 def get_files(path)
   files = []
   Find.find(path) do |c|
-    files << File.basename(c, ".erb") if !File.directory?(c)
+    files << File.basename(c, ".md") if !File.directory?(c)
   end
   return files
 end
@@ -16,14 +22,11 @@ get '/' do
 end
 
 get '/visualizar/:post/?' do
-  halt 404 unless File.exist?("views/posts/#{params[:post]}.erb")
-  erb :"posts/#{params[:post]}"
-end
-
-get '/sobre-o-autor/?' do
-  erb :author
+  halt 404 unless File.exist?("views/posts/#{params[:post]}.md")
+  markdown :"posts/#{params[:post]}"
 end
 
 not_found do
   erb :"404"
 end
+
